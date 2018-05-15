f3d1781f8933370661d1e458f5889e52b81dd069
ach74 <32370243+ach74@users.noreply.github.com>
2018-05-16 01:38:37 +0200

    ach

--- js/js.js
+++ js/js.js
@@ -30,19 +30,52 @@ $(document).ready(function(){
 function giveMeMore() {
 
 //alert("The paragraph was clicked." + moreNew);
-	
-	if (moreNew < 6) {
-		$.getJSON("https://rawgit.com/ach74/PaginaNoticias/master/json/" + moreNew + ".json", function (a) {
-			addNew(a);
-		}); moreNew++;
-	} else {
-		$('#mas').text('No hay más noticias');
-	}
+
+if (moreNew < 6) {
+	$.getJSON("https://rawgit.com/ach74/PaginaNoticias/master/json/" + moreNew + ".json", function (a) {
+		addNew(a);
+	}); moreNew++;
+} else {
+	$('#mas').text('No hay más noticias');
+}
 };
 
 function addNew(json) {
+	var demo = 4;
 	$.each(json, function (b, item) {
 		$(".noticias").append(
+			'<div class="col-sm-4 col-md-4">'+
+			'<div class="thumbnail">' +
+
+					'<a href="#" class="navOpcion">' +
+						'<div class="caption">' + '<h3 class="text-justify">' + item.titulo + "</h3>" + "</div>" +
+						'<img src="' + item.imgmid + '" alt="..." />' +
+					'</a>' +
+
+				'<div class="caption">' + '<p class="text-justify">' + item.descripcion + "</p>" +
+					'<div class="container-fluid">' + 
+						'<button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo'+demo+'">Info</button>'+
+							'<div class="glyphicon glyphicon-heart"></div>'+
+							'<div class="glyphicon glyphicon-retweet"></div>'+
+					'</div>'+
+					'<div id="demo'+demo+'" class="collapse">'+ 
+		 				'<hr>'+
+						'<div class="container">'+
+							'<div class="media">'+
+								'<div class="media-left">'+
+									'<img src="img/img_avatar1.png" class="media-object" style="width:45px">'+
+								'</div>'+ 
+								'<div class="media-body">'+
+									'<h4 class="media-heading">Joe Russo <br><small><i>Posted on April 27, 2018</i></small></h4>'+
+									'<button type="button" class="btn btn-warning btn-sm">Acción</button><button type="button" class="btn btn-danger btn-sm">Aventura</button>'+
+								'</div>'+ 
+							'</div>'+ 
+						'</div>'+ 
+					'</div>'+
+				'</div>'+
+			'</div>'
+
+			/*
 			'<div class="col-sm-4 col-md-4">' +
 			'<div class="thumbnail">' +
 			'<a href="#" class="navOpcion">' +
@@ -51,6 +84,7 @@ function addNew(json) {
 			'</a>' +
 			'<div class="caption">' + '<p class="text-justify">' + item.descripcion + "</p>" +
 			'<div class="container-fluid">' +
+
 			'<button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo1">'+Info+'</button>'+
 			'<div class="glyphicon glyphicon-heart">'+'</div>'+
 			'<div class="glyphicon glyphicon-retweet">'+'</div>'+
@@ -63,10 +97,12 @@ function addNew(json) {
 			'<img src="img/img_avatar1.png" class="media-object" style="width:45px">' + 
 			'</div>' +
 			'<div class="media-body">'+
-			'<h4 class="media-heading">'+Rick Jam +'<br>'+'<small>'+'<i>'+Posted on + item.fecha.month + ' ' + item.fecha.day + ' ' + item.fecha.year+'</i>'+'</small>'+'</h4>'+
-			'<button type="button" class="btn btn-warning btn-sm">'+ Animation+'</button>'+
-			'<button type="button" class="btn btn-info btn-sm">'+Comedy+'</button>'+
-		 	'<button type="button" class="btn btn-danger btn-sm">'+Adventure+'</button>'+
-		 	'</div>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>');
+			'<h4 class="media-heading">'+'Rick Jam' +'<br>'+'<small>'+'<i>'+ 'Posted on' + item.fecha.month + ' ' + item.fecha.day + ' ' + item.fecha.year+'</i>'+'</small>'+'</h4>'+
+			'<button type="button" class="btn btn-warning btn-sm">'+ 'Animation'+'</button>'+
+			'<button type="button" class="btn btn-info btn-sm">'+'Comedy'+'</button>'+
+		 	'<button type="button" class="btn btn-danger btn-sm">'+'Adventure'+'</button>'+
+		 	'</div>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>'
+		 	*/);
+		demo++;
 	})
 };
