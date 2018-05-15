fb4419b11098e1c9c68ab6b6c8eb56e5c247bb42
ach74 <32370243+ach74@users.noreply.github.com>
2018-05-16 01:07:37 +0200

    ach

--- js/js.js
+++ js/js.js
@@ -32,21 +32,41 @@ function giveMeMore() {
 //alert("The paragraph was clicked." + moreNew);
 	
 	if (moreNew < 6) {
-		$.getJSON("https://github.com/ach74/PaginaNoticias/tree/master/json/" + moreNew + ".json", function (jsonObject) {
-			addrow(jsonObject);
+		$.getJSON("https://rawgit.com/ach74/PaginaNoticias/master/json/" + moreNew + ".json", function (a) {
+			addNew(a);
 		}); moreNew++;
 	} else {
 		$('#mas').text('No hay más noticias');
 	}
 };
 
-function addrow(json) {
-	$.each(json, function (i, item) {
-		$(".noticias").append('<div class="col-sm-4 col-md-4">' +
+function addNew(json) {
+	$.each(json, function (b, item) {
+		$(".noticias").append(
+			'<div class="col-sm-4 col-md-4">' +
 			'<div class="thumbnail">' +
+			'<a href="#" class="navOpcion">' +
 			'<div class="caption">' + '<h3 class="text-justify">' + item.titulo + "</h3>" + "</div>" +
 			'<img src="' + item.imgmid + '" alt="..." />' +
+			'</a>' +
 			'<div class="caption">' + '<p class="text-justify">' + item.descripcion + "</p>" +
-			'<p class="text-right">' + "<em>" + item.fecha + "</em>" + "</p>" + "</div>" + "</div>" + "</div>");
+			'<div class="container-fluid">' +
+			'<button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo1">'+Info+'</button>'+
+			'<div class="glyphicon glyphicon-heart">'+'</div>'+
+			'<div class="glyphicon glyphicon-retweet">'+'</div>'+
+			'</div>'+
+			'<div id="demo1" class="collapse">'+ 
+			'<hr>' +
+			'<div class="container">'+
+			'<div class="media">'+
+			'<div class="media-left">'+
+			'<img src="img/img_avatar1.png" class="media-object" style="width:45px">' + 
+			'</div>' +
+			'<div class="media-body">'+
+			'<h4 class="media-heading">'+Rick Jam +'<br>'+'<small>'+'<i>'+Posted on + item.fecha.month + ' ' + item.fecha.day + ' ' + item.fecha.year+'</i>'+'</small>'+'</h4>'+
+			'<button type="button" class="btn btn-warning btn-sm">'+ Animation+'</button>'+
+			'<button type="button" class="btn btn-info btn-sm">'+Comedy+'</button>'+
+		 	'<button type="button" class="btn btn-danger btn-sm">'+Adventure+'</button>'+
+		 	'</div>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>');
 	})
 };
